bca36d85805bb3049d1abdf5d0342b2a8195651f
0k0se <okose0732@icloud.com>
2026-08-30 11:14:19 +0300

    test controls after audio policy exhaustion

---
+++ scripts/check-audio-control-policy-restart-exhaustion-qemu.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+set -eu
+
+ROOT_DIR="$(CDPATH= cd -- "$(dirname "$0")/.." && pwd)"
+
+AQUA_AUDIO_QEMU_CONTRACT=control-policy-restart-exhaustion \
+    AQUA_AUDIO_QEMU_EVIDENCE_DIR="${AQUA_AUDIO_QEMU_EVIDENCE_DIR:-${ROOT_DIR}/build/audio-control-policy-restart-exhaustion-qemu}" \
+    exec "${ROOT_DIR}/scripts/check-audio-qemu.sh" "$@"
